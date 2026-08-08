64b556e178b23abc8b8144d362cc04fe9034fb51
Jason Mac <jasonvanmac@gmail.com>
2026-08-08 13:24:26 -0400

    [feat] add thread pool and tests

--- worker/src/thread_pool.cpp
+++ worker/src/thread_pool.cpp
@@ -1,1 +1,50 @@
 #include "thread_pool.h"
+
+ThreadPool::ThreadPool()
+{
+  unsigned int numThreads = std::max(std::thread::hardware_concurrency(), NUM_DEFAULT_THREADS);
+  for (unsigned int i = 0; i < numThreads; ++i)
+  {
+    thread_pool_.push_back(spawnWorkerThread());
+  }
+}
+
+ThreadPool::~ThreadPool()
+{
+  stop_source_.request_stop();
+}
+
+std::jthread ThreadPool::spawnWorkerThread()
+{
+  const auto worker_thread = [this]() -> void
+  {
+    for (;;)
+    {
+      std::unique_lock<std::mutex> lock(this->mutex_);
+      if (!this->task_available_.wait(lock, this->stop_source_.get_token(),
+                                      [this] { return !this->task_queue_.empty(); }))
+      {
+        return;
+      }
+      auto task = this->task_queue_.front();
+      this->task_queue_.pop();
+      lock.unlock();
+      task();
+    }
+  };
+  return std::jthread(worker_thread);
+}
+
+unsigned int ThreadPool::getNumThreads()
+{
+  return thread_pool_.size();
+}
+
+void ThreadPool::submit(std::function<void()> task)
+{
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    task_queue_.push(task);
+  }
+  task_available_.notify_one();
+}
